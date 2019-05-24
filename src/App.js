4304e1df2babec0a79f5e28cf01b2315a347867f
Sampson Mui <trendzme@gmail.com>
2019-05-24 14:50:03 -0700

    initial update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './styles/App.css';
-import { Link, Switch, Route } from 'react-router-dom';
-
-
+import React from "react";
+import "./styles/App.css";
+import { Link, Switch, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <div>THIS IS OUR APP</div>
+      {/* <div>THIS IS OUR APP</div> */}
+      <div className="margin--left">
+        <Header />
+        <Main />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>SCIENCE</h1>
+      {/* <p className="header__description">Header Description</p> */}
+    </div>
+  );
+}
